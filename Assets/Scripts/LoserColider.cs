a862e05a44522fcae1790cd29661e2473ebd01f2
tommy-tran <tome7@live.com>
2017-06-16 04:17:07 -0400

    loserCollider functionality to trigger on ball drop

---
+++ Assets/Scripts/LoserColider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoserColider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print("Trigger");
+		levelManager.LoadLevel ("Win Screen");
+	}
+
+	void OnCollisionEnter2D (Collider2D collision) {
+		print ("Collision");
+	}
+
+}
